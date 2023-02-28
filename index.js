803549d353e486f549b47045f7ac0d5d4f39352f
naveen-redd <19311A0516@sreenidhi.edu.in>
2023-02-28 15:40:03 +0530

    first load change

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// parent class for card
+// parent element to store cards
 const taskContainer= document.querySelector(".task_container");
+// GLobal store
+const globalStore=[];
 const newCard=({id,imageUrl,taskTitle,taskDescription,taskType})=>`<div class="col-md-6 col-lg-4' id=${id}>
     <div class="card">
     <div class="card-header d-flex justify-content-end gap-2">
@@ -17,7 +19,25 @@ const newCard=({id,imageUrl,taskTitle,taskDescription,taskType})=>`<div class="c
     <div class="card-footer text-muted"><button type="button" class="btn btn-outline-primary float-end">Open Task</button>
     </div>
   </div>
-</div>`;
+  </div>`;
+
+const loadTaskCards=()=>{
+  // access localStorage
+
+  const getInitialData=localStorage.getItem("tasky");
+  if(!getInitialData) return;
+  //convert string object to object
+
+  const {cards }=JSON.parse(getInitialData);
+  // map around the array to generate HTML card and inject to DOM
+
+  cards.map((card)=>{
+    const createNewcard=newCard(card);
+    taskContainer.insertAdjacentHTML("beforeend",createNewcard);
+    globalStore.push(card);
+  });
+}
+// to get dynamic data from website
 const saveChanges=()=>{
     const taskdata={
         id:`${Date.now()}`, //unique number for card id
@@ -26,7 +46,13 @@ const saveChanges=()=>{
         taskDescription:document.getElementById("taskdesc").value,
         taskType:document.getElementById("tasktype").value,
     };
-
+    //HTML code
     const createNewcard= newCard(taskdata);
     taskContainer.insertAdjacentHTML("beforeend",createNewcard);
+    globalStore.push(taskdata);
+
+    // calling API to save in local storage
+    localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
 };
+
+//storing cards in local storage(5MB)
